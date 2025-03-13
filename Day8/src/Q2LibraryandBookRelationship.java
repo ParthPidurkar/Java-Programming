8f186a786368b68035f60ed55bfd830485fe9132
ParthPidurkar <parthpidurkar667@gmail.com>
2025-03-13 22:30:05 +0530

    13/03/2024

--- Day8/src/Q2LibraryandBookRelationship.java
+++ Day8/src/Q2LibraryandBookRelationship.java
@@ -1,4 +1,40 @@
+class Library 
+{
+	String name ;
+	String address ;
+	Book books ;
+	
+	Library(String name,String address)
+	{
+		this.name =name;
+		this.address = address;
+		
+	}
+	
+	void addBook(Book books)
+	{
+		this.books = books;
+	}
+	
+}
 
-public class Q2LibraryandBookRelationship {
+class Book
+{
+	String title ;
+	String author ;
+	String isbn;
+	Book(String title,String author, String isbn)
+	{
+		this.title =title;
+		this.author = author;
+		this.isbn = isbn;
+	}
+}
+
+
+
+
+public class Q2LibraryandBookRelationship
+{
 
 }
